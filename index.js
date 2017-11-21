e24d17f6bb61a31dab6b5bad98c8008bff61ea60
“pangff” <admin@example.com>
2017-11-21 17:11:18 +0800

    fix bug

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const Pageres = require('pageres');
-const filenamifyUrl = require('filenamify-url');
-const path = require('path');
-let url = filenamifyUrl("http://m.feawin.com")
-const pageres = new Pageres({delay: 10})
-    .src(url, ['480x320', '1024x768', 'iphone 5s'])
-    .dest(path.join(__dirname,"./images"))
-    .run()
-    .then(() => console.log('done'));
+var casper = require('casper').create();
+
+casper.start('http://m.feawin.com', function() {
+    this.captureSelector('twitter.png', '#twitter-block');
+});
+casper.run();
+//const filenamifyUrl = require('filenamify-url');
+//const path = require('path');
+//let url = filenamifyUrl("http://m.feawin.com")
+//const pageres = new Pageres({delay: 10})
+//    .src(url, ['480x320', '1024x768', 'iphone 5s'])
+//    .dest(path.join(__dirname,"./images"))
+//    .run()
+//    .then(() => console.log('done'));
+
