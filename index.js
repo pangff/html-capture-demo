db06b6ee6b40e9e0d7ccc45febf47901f5b4b4b9
“pangff” <admin@example.com>
2017-11-21 16:14:42 +0800

    fix bug

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Pageres = require('pageres');
 
 const pageres = new Pageres({delay: 5})
-    .src('m.feawin.com!scene!client!share.html', ['480x320', '1024x768', 'iphone 5s'],{filename:"test"})
+    .src('m.feawin.com!scene/client/share.html', ['480x320', '1024x768', 'iphone 5s'],{filename:"test"})
     .dest(__dirname)
     .run()
     .then(() => console.log('done'));
