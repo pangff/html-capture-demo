bb9f7ddcc2d9d2549ac95f50c3bd8001b26e882a
“pangff” <admin@example.com>
2017-11-21 16:29:15 +0800

    fix bug

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Pageres = require('pageres');
 const filenamifyUrl = require('filenamify-url');
+const path = require('path');
 let url = filenamifyUrl("http://m.feawin.com/scene/client/share.html")
 const pageres = new Pageres({delay: 10})
     .src(url, ['480x320', '1024x768', 'iphone 5s'])
