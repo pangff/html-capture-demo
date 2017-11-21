e0340b5823148c55ab62a75b5fdecbccc41fddb8
“pangff” <admin@example.com>
2017-11-21 16:21:51 +0800

    fix bug

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Pageres = require('pageres');
 const filenamifyUrl = require('filenamify-url');
 let url = filenamifyUrl("http://www.baidu.com")
 const pageres = new Pageres({delay: 10})
-    .src(url, ['480x320', '1024x768', 'iphone 5s'],{filename:"test"})
+    .src(url, ['480x320', '1024x768', 'iphone 5s'])
     .dest(__dirname)
     .run()
     .then(() => console.log('done'));
