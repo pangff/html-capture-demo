ad3e90f2021f1f7b59cdaf6aa54595a6ded13b04
“pangff” <admin@example.com>
2017-11-21 16:02:45 +0800

    fix bug

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const Pageres = require('pageres');
 
 const pageres = new Pageres({delay: 2})
-    .src('yeoman.io', ['480x320', '1024x768', 'iphone 5s'], {crop: true})
-    .src('todomvc.com', ['1280x1024', '1920x1080'])
-    .src('data:text/html;base64,PGgxPkZPTzwvaDE+', ['1024x768'])
+    .src('m.feawin.com!scene!client!share.html', ['480x320', '1024x768', 'iphone 5s'])
     .dest(__dirname)
     .run()
     .then(() => console.log('done'));
