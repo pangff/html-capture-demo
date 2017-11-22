7d1ef85eef11e50204d3be050ba96f1cfe87161f
“pangff” <admin@example.com>
2017-11-22 15:48:22 +0800

    fix bug

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ server.get('/twitter/get', function(req, res, next){
     let url = req.params.url;
     let filename = md5(url)
     url = encodeURIComponent(url)
-    let requestUrl = filenamifyUrl("http://localhost/twitter")+"!url="+url;
+    let requestUrl = filenamifyUrl("https://localhost/twitter")+"!url="+url;
     console.log("requestUrl:"+requestUrl)
     new Pageres({delay: 1})
         .src(requestUrl, ['480x320'],{selector:"#twitter-widget-0",transparent:true,filename:filename})
