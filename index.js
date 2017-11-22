e15db6384ac465627b391bfce7df4967c6da4b09
“pangff” <admin@example.com>
2017-11-22 14:24:36 +0800

    fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Pageres = require('pageres');
 
 const pageres = new Pageres({delay: 2})
-    .src('m.feawin.com', ['480x320'],{selector:".EmbeddedTweet-tweet"})
+    .src('m.feawin.com', ['480x320'],{selector:".EmbeddedTweet"})
     .dest(__dirname)
     .run()
     .then(() => console.log('done'));
