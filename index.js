895b6b55268c6123f04462c2040b094badc14342
“pangff” <admin@example.com>
2017-11-22 14:40:34 +0800

    fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Pageres = require('pageres');
 
 const pageres = new Pageres({delay: 2})
-    .src('m.feawin.com', ['480x320'],{selector:"div.SandboxRoot"})
+    .src('m.feawin.com', ['480x320'],{selector:"#shadow-root"})
     .dest(__dirname)
     .run()
     .then(() => console.log('done'));
