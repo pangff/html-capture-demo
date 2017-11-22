6bbc19ade66b316286a5214f064c12c443e98fb5
“pangff” <admin@example.com>
2017-11-22 14:02:18 +0800

    fix bug

--- index.js
+++ index.js
@@ -1,33 +1,7 @@
-const casper = require('casper').create();
+const Pageres = require('pageres');
 
-casper.start('http://m.feawin.com', function() {
-
-});
-// listener function for requested resources
-var listener = function() {
-
-    var startTime = Date.now();
-    for(var i=startTime;(i-startTime)<5000;i=Date.now()){
-
-    }
-    this.captureSelector('twitter2.png', '#twitter-block');
-    //var _this = this;
-    //this.wait(2000,function(){
-    //    console.log("22222")
-    //    _this.captureSelector('twitter2.png', '#twitter-block');
-    //});
-};
-
-// listening to all resources requests
-casper.on("load.finished", listener);
-
-casper.run();
-//const filenamifyUrl = require('filenamify-url');
-//const path = require('path');
-//let url = filenamifyUrl("http://m.feawin.com")
-//const pageres = new Pageres({delay: 10})
-//    .src(url, ['480x320', '1024x768', 'iphone 5s'])
-//    .dest(path.join(__dirname,"./images"))
-//    .run()
-//    .then(() => console.log('done'));
-
+const pageres = new Pageres({delay: 2})
+    .src('m.feawin.com', ['480x320', '1024x768', 'iphone 5s'],{selector:"#twitter-block"})
+    .dest(__dirname)
+    .run()
+    .then(() => console.log('done'));
