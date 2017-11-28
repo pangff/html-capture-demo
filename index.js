903bdba9a6f0bdf14762a6dc50f9c40ce8893c05
“pangff” <admin@example.com>
2017-11-28 13:12:08 +0800

    fix bug

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ server.get('/twitter/get', function(req, res, next){
         captureTag = "#plc_main";
         requestUrl = url;
     }else if(myURL.host=="www.facebook.com"){
-        captureTag = "._1dwg _1w_m _q7o";
+        captureTag = "div._1dwg _1w_m._q7o";
         requestUrl = url;
     }else{
         captureTag = "._1dwg _1w_m _q7o";
