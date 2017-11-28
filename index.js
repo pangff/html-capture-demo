e09f05d302cb79b8b521607676d8e59e5eb91c72
“pangff” <admin@example.com>
2017-11-28 13:57:20 +0800

    fix bug

--- index.js
+++ index.js
@@ -31,48 +31,72 @@ server.use(plugins);
 server.get('/:url', function(req, res, next){
 
     let url = req.params.url;
-
-
-
     url = new Buffer(url,"base64").toString();
-    axios.get(url).then((result)=>{
-        return result.data;
-    }).then((result)=>{
-        if(result.html){
-            let html = `<!DOCTYPE html>
+
+    const myURL = new URL(url);
+
+    if(myURL.host=="www.facebook.com"){
+        let html = `<html>
+                    <title>My Website</title>
+                    <body>
+                    <script src="//connect.facebook.net/en_US/sdk.js#xfbml=1&amp;version=v2.5"
+                    async></script>
+                    <div class="fb-post"
+                    data-href="https://www.facebook.com/20531316728/posts/10154009990506729/"
+                    data-width="500"></div>
+                        </body>
+                        </html>`;
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        res.write(results);
+        res.end();
+        return next();
+    }else{
+        axios.get(url).then((result)=>{
+            return result.data;
+        }).then((result)=>{
+            if(result.html){
+
+                if(myURL.host=="www.twitter.com"){
+                    let html = `<!DOCTYPE html>
                         <html>
                         <head>
                             <meta charset="UTF-8">
                         </head>
                         <body id="body">`
-            let end = `</body></html>`;
-            let results = html+result.html+end;
-            console.log(results)
+                    let end = `</body></html>`;
+                    let results = html+result.html+end;
+                    console.log(results)
 
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
+                    res.writeHead(200, {
+                        'Content-Type': 'text/html'
+                    });
 
-            res.write(results);
-            res.end();
-            return next();
-        }else{
+                    res.write(results);
+                    res.end();
+                    return next();
+                }
+
+            }else{
+                res.writeHead(200, {
+                    'Content-Type': 'text/html'
+                });
+                res.write("");
+                res.end();
+                return next();
+            }
+        }).catch((e)=>{
             res.writeHead(200, {
                 'Content-Type': 'text/html'
             });
             res.write("");
             res.end();
             return next();
-        }
-    }).catch((e)=>{
-        res.writeHead(200, {
-            'Content-Type': 'text/html'
-        });
-        res.write("");
-        res.end();
-        return next();
-        console.log(e)
-    })
+            console.log(e)
+        })
+    }
+
 })
 
 
@@ -99,7 +123,8 @@ server.get('/twitter/get', function(req, res, next){
         requestUrl = url;
     }else if(myURL.host=="www.facebook.com"){
         captureTag = "div._1dwg._1w_m._q7o";
-        requestUrl = url;
+        let urlBase64 = new Buffer(url).toString("base64");
+        requestUrl= "http://localhost/"+urlBase64;
     }else{
         captureTag = "._1dwg _1w_m _q7o";
         requestUrl = url;
